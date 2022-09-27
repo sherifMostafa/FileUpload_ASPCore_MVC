a889d8b07bcfbf32b6275e17bc1570b3661b767e
Sherif Mostafa <sherifcrew94@mgail.com>
2022-09-27 10:21:11 +0200

    Example For Upload File In Asp Core

---
+++ FileUpload/Controllers/FileUpload.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class FileUpload : Controller
+    {
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost("uploadfile")]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+            var size = files.Sum(f => f.Length);
+            var filePaths = new List<string>();
+
+
+            foreach (var formFile in files)
+            {
+                if(formFile.Length > 0)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), formFile.FileName);
+                    filePaths.Add(filePath);
+
+                    using (var stream = new FileStream(filePath , FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+
+                }
+            }
+            return Ok(new {files.Count , filePaths});
+        }
+    }
+}
